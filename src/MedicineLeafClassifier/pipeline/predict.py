403c25df7ec7bbc87a319ae6d8b06c20e7adf8bb
Chaitanya-Endurance <kvschaitanya17@gmail.com>
2024-03-03 19:03:53 +0530

    prediction

--- src/MedicineLeafClassifier/pipeline/predict.py
+++ src/MedicineLeafClassifier/pipeline/predict.py
@@ -1,6 +1,7 @@
 import numpy as np
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
+import tensorflow as tf 
+# from tensorflow.keras.saving import load_model  
+# from tensorflow.keras.utils import image
 import os
 
 classnames = os.listdir(os.path.join("artifacts","data_ingestion","indian-medicinal-leaf-image-dataset","Medicinal Leaf dataset"))
@@ -12,15 +13,20 @@ class PredictPipeline:
 
     def predict(self):
         #load_model 
-        model = load_model(os.path.join("artifacts","training","model.h5"))
+        model = tf.keras.saving.load_model(os.path.join("artifacts","training","model.h5"))
 
 
         imagename = self.filename
-        test_image = image.load_img(imagename,target_size = (224,224))
-        test_image = image.img_to_array(test_image)
+        test_image = tf.keras.utils.image.load_img(imagename,target_size = (224,224))
+        test_image = tf.keras.utils.image.img_to_array(test_image)
         test_image = np.expand_dims(test_image, axis = 0)
-        result = np.argmax(model.predict(test_image),axis = 1)
-        print(classnames[result])
+        result = model.predict(test_image,verbose = 0)
+        predicted_class = classnames[np.argmax(result[0])]
+        confidence = round(100 * (np.max(result[0])),2)
+        # result = np.argmax(model.predict(test_image),axis = 1)
+        
+
+        return predicted_class,confidence
 
         
 
